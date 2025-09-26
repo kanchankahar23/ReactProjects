9117cdb904005c5d853883d89f4f78e1c6086a90
kanchan kahar <kaharkanchan884@gmail.com>
2025-09-26 14:08:55 +0530

    feat: initialize 05card project with Vite, React, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Included Vite configuration for React and Tailwind CSS
    - Created main application structure with App component
    - Added global styles with Tailwind CSS
    - Included React and Vite logos as SVG assets
    - Removed previous package-lock.json and package.json

---
+++ 04Card/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center text-purple-600 mt-10">
+      Tailwind working with Plugin 🚀
+    </h1>
+  )
+}
+export default App
---
+++ 05card/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-zinc-800'>
+    <h1 className="bg-zinc-700 text-white">Hello world!</h1>
+    <h1 className="text-9xl font-extrabold text-white">Kanchan Kahar</h1>
+    </div>
+  )
+}
+
+export default App
