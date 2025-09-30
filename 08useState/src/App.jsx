7fc8a477b08ff14470ce552c3edabe9be941289b
kanchan kahar <kaharkanchan884@gmail.com>
2025-09-30 14:04:15 +0530

    fix: add isbanned property to name state and update change button logic

--- 08useState/src/App.jsx
+++ 08useState/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   }
 
   const [val, setVal] = useState({ name: "jo", age: 22 })
-  const [name, setName] = useState({"name":"rose","age":22})
+  const [name, setName] = useState({"name":"rose","age":22,isbanned:false});
     return (
     <div className='p-20'>
       <h1 className='font-bold'>{count}</h1>
@@ -26,10 +26,17 @@ const App = () => {
 
 <h1>name: {name.name}</h1>
 <h1> age: {name.age}</h1>
+<h1>isbanned: {name.isbanned.toString()}</h1>
   
   <button onClick={()=>{
-    setName({...name,name:"kanchan"})
-  }} className='px-4 py-1 rounded-full text-white bg-pink-950'>change</button>
+    setName({...name,name:"kanchan",
+      isbanned:!name.isbanned
+    })
+
+  }} className={`px-4 py-1 rounded-full text-white ${name.isbanned ? "bg-blue-900" :"bg-red-900"}`}>change</button>
+
+
+
     </div>
   )
 }
