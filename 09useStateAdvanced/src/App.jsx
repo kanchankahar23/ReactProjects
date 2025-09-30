7290640b8d9e26777ac2b39ebd2ecea57c556b43
kanchan kahar <kaharkanchan884@gmail.com>
2025-09-30 16:56:37 +0530

    chore: add package-lock.json with initial dependencies and configurations

---
+++ 09useStateAdvanced/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function App() {
+      // array
+    const [val,setVal]  = useState([1,2,3,4,5,6,7]);
+    const [val2,setVal2] = useState([10,11,12,13,14,15,16,17,18]) ;
+     return (
+
+    <div className='p-20'>
+      {val.map((item)=><h1>{item}</h1>)}
+      <button onClick={()=>{setVal(()=>{
+        return val.filter((item,index)=>index!=val.length-1)
+      })}} className='px-3 py-1 rounded-full bg-blue-600 text-white'>click me</button>
+
+
+      {val2.map((item)=><h1>{item}</h1>)}
+      <button onClick={()=>{setVal2(()=>{
+        return val2.filter((item,index)=>index%2==0)
+      })}}
+
+       className='px-3 py-1 rounded-full bg-red-600 text-white'>EVEN</button>
+    </div>
+    
+  )
+}
+
+export default App
